edc9a98585fc8d7d288d9d4dbaedaef08cfcae5e
Bence Balogh <bence.balogh@intland.com>
2022-09-29 11:15:35 +0200

    add froala to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const FroalaEditor: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-froala-test';
+
+  ngOnInit(): void {
+    new FroalaEditor('div#froala-editor')
+  }
 }
