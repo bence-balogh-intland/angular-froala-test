31e2e8faf2d5e3c35de3bf84dbe48b602525170d
Bence Balogh <bence.balogh@intland.com>
2022-09-29 11:50:57 +0200

    add custom icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,28 @@ declare const FroalaEditor: any;
 export class AppComponent implements OnInit{
   title = 'angular-froala-test';
 
+  froalaOptions: any;
+
   ngOnInit(): void {
-    new FroalaEditor('div#froala-editor')
+
+    FroalaEditor.DefineIcon('myIcon', { NAME: 'my-icon', PATH: 'M7 0 L7 20 L22 20 Z'})
+
+    FroalaEditor.RegisterCommand('myIcon', {
+      title: 'My Custom Icon',
+      callback: function () {
+      },
+    })
+
+    this.froalaOptions = {
+      toolbarButtons: {
+        ...FroalaEditor.TOOLBAR_BUTTONS,
+        moreText: {
+          ...FroalaEditor.TOOLBAR_BUTTONS.moreText,
+          buttons: ['myIcon', ...FroalaEditor.TOOLBAR_BUTTONS.moreText.buttons]
+        }
+      }
+    };
+
+    new FroalaEditor('div#froala-editor', this.froalaOptions)
   }
 }
